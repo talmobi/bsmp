498f6b0245a49d3c07fd6b8d32f89954552f9024
talmobi <talmo.christian@gmail.com>
2025-10-11 14:32:47 +0300

    demo: update color every 1 second for demo purposes

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,18 @@
 
 var el = document.getElementById( 'root' )
 
-var colors = [ 'pink', 'salmon', 'orange', 'tomato', 'yellow' ]
+function update () {
+  var colors = [ 'pink', 'salmon', 'orange', 'tomato', 'yellow' ]
 
-var n = Math.floor( colors.length * Math.random() )
-var color = colors[ n ]
-
-el.style.color = color
-
+  var n = Math.floor( colors.length * Math.random() )
+  var color = colors[ n ]
+  el.style.color = color
 el.textContent = color + ' cats and dogs.'
+}
+update()
+
+function tick () {
+  update()
+  setTimeout(tick, 1000 / 1)
+}
+tick()
